7155ea3625b24bc6348e6db74fd8598de53522dc
Ted Rice <ted.rice@gmail.com>
2022-10-10 12:34:01 -0600

    fix: Add tokens from WOTLK Phase 1

--- Libs/LibItemUtil-1.1/Items.lua
+++ Libs/LibItemUtil-1.1/Items.lua
@@ -117,21 +117,21 @@ lib.TokenEquipmentLocations = {
     [30245] = { "LegsSlot" },                   -- Leggings of the Vanquished Champion
     [32405] = { "NeckSlot"} ,                    -- Verdant Sphere
     -- TBC Classic P3 (T6)
-    [31097] = { "HeadSlot"},                    -- Helm of the Forgotten Conqueror
-    [31095] = { "HeadSlot"},                    -- Helm of the Forgotten Protector
-    [31096] = { "HeadSlot"},                    -- Helm of the Forgotten Vanquisher
-    [31101] = { "ShoulderSlot"},                -- Pauldrons of the Forgotten Conqueror
-    [31103] = { "ShoulderSlot"},                -- Pauldrons of the Forgotten Protector
-    [31102] = { "ShoulderSlot"},                -- Pauldrons of the Forgotten Vanquisher
-    [31089] = { "ChestSlot"},                   -- Chestguard of the Forgotten Conqueror
-    [31091] = { "ChestSlot"},                   -- Chestguard of the Forgotten Protector
-    [31090] = { "ChestSlot"},                   -- Chestguard of the Forgotten Vanquisher
-    [31092] = { "HandsSlot"},                   -- Gloves of the Forgotten Conqueror
-    [31094] = { "HandsSlot"},                   -- Gloves of the Forgotten Protector
-    [31093] = { "HandsSlot"},                   -- Gloves of the Forgotten Vanquisher
-    [31098] = { "LegsSlot"},                    -- Leggings of the Forgotten Conqueror
-    [31100] = { "LegsSlot"},                    -- Leggings of the Forgotten Protector
-    [31099] = { "LegsSlot"},                    -- Leggings of the Forgotten Vanquisher
+    [31097] = { "HeadSlot" },                   -- Helm of the Forgotten Conqueror
+    [31095] = { "HeadSlot" },                   -- Helm of the Forgotten Protector
+    [31096] = { "HeadSlot" },                   -- Helm of the Forgotten Vanquisher
+    [31101] = { "ShoulderSlot" },               -- Pauldrons of the Forgotten Conqueror
+    [31103] = { "ShoulderSlot" },               -- Pauldrons of the Forgotten Protector
+    [31102] = { "ShoulderSlot" },               -- Pauldrons of the Forgotten Vanquisher
+    [31089] = { "ChestSlot" },                  -- Chestguard of the Forgotten Conqueror
+    [31091] = { "ChestSlot" },                  -- Chestguard of the Forgotten Protector
+    [31090] = { "ChestSlot" },                  -- Chestguard of the Forgotten Vanquisher
+    [31092] = { "HandsSlot" },                  -- Gloves of the Forgotten Conqueror
+    [31094] = { "HandsSlot" },                  -- Gloves of the Forgotten Protector
+    [31093] = { "HandsSlot" },                  -- Gloves of the Forgotten Vanquisher
+    [31098] = { "LegsSlot" },                   -- Leggings of the Forgotten Conqueror
+    [31100] = { "LegsSlot" },                   -- Leggings of the Forgotten Protector
+    [31099] = { "LegsSlot" },                   -- Leggings of the Forgotten Vanquisher
     -- TBC Classic P3 (T6)
     [34848] = { "WristSlot" },                  -- Bracers of the Forgotten Conqueror
     [34851] = { "WristSlot" },                  -- Bracers of the Forgotten Protector
@@ -142,10 +142,43 @@ lib.TokenEquipmentLocations = {
     [34853] = { "WaistSlot" },                  -- Belt of the Forgotten Conqueror
     [34854] = { "WaistSlot" },                  -- Belt of the Forgotten Protector
     [34855] = { "WaistSlot" },                  -- Belt of the Forgotten Vanquisher
+    -- WOTLK Classic P1 (T7)
+    -- 10 player
+    [40616] = { "HeadSlot" },                   -- Helm of the Lost Conqueror
+    [40617] = { "HeadSlot" },                   -- Helm of the Lost Protector
+    [40618] = { "HeadSlot" },                   -- Helm of the Lost Vanquisher
+    [40613] = { "HandsSlot" },                  -- Gloves of the Lost Conqueror
+    [40614] = { "HandsSlot" },                  -- Gloves of the Lost Protector
+    [40615] = { "HandsSlot" },                  -- Gloves of the Lost Vanquisher
+    [40610] = { "ChestSlot" },                  -- Chestguard of the Lost Conqueror
+    [40611] = { "ChestSlot" },                  -- Chestguard of the Lost Protector
+    [40612] = { "ChestSlot" },                  -- Chestguard of the Lost Vanquisher
+    [40619] = { "LegsSlot" },                   -- Leggings of the Lost Conqueror
+    [40620] = { "LegsSlot" },                   -- Leggings of the Lost Protector
+    [40621] = { "LegsSlot" },                   -- Leggings of the Lost Vanquisher
+    [40622] = { "ShoulderSlot" },               -- Spaulders of the Lost Conqueror
+    [40623] = { "ShoulderSlot" },               -- Spaulders of the Lost Protector
+    [40624] = { "ShoulderSlot" },               -- Spaulders of the Lost Vanquisher
+    -- 25 player
+    [40631] = { "HeadSlot" },                   -- Crown of the Lost Conqueror
+    [40632] = { "HeadSlot" },                   -- Crown of the Lost Protector
+    [40633] = { "HeadSlot" },                   -- Crown of the Lost Vanquisher
+    [40628] = { "HandsSlot" },                  -- Gauntlets of the Lost Conqueror
+    [40629] = { "HandsSlot" },                  -- Gauntlets of the Lost Protector
+    [40630] = { "HandsSlot" },                  -- Gauntlets of the Lost Vanquisher
+    [40625] = { "ChestSlot" },                  -- Breastplate of the Lost Conqueror
+    [40626] = { "ChestSlot" },                  -- Breastplate of the Lost Protector
+    [40627] = { "ChestSlot" },                  -- Breastplate of the Lost Vanquisher
+    [40634] = { "LegsSlot" },                   -- Legplates of the Lost Conqueror
+    [40635] = { "LegsSlot" },                   -- Legplates of the Lost Protector
+    [40636] = { "LegsSlot" },                   -- Legplates of the Lost Vanquisher
+    [40637] = { "ShoulderSlot" },               -- Mantle of the Lost Conqueror
+    [40638] = { "ShoulderSlot" },               -- Mantle of the Lost Protector
+    [40639] = { "ShoulderSlot" },               -- Mantle of the Lost Vanquisher
 }
 
 -- mapping from token to items (id) which are rewarded from turning in
--- currently only has TBC Classic P1 and on
+-- currently only has TBC Classic P1 and onwards
 lib.TokenItems = {
     -- TBC Classic P1 (T4)
     [29761] = { 29011, 29021, 29049, 29058, 29086, 29093, 29098 },  -- Helm of the Fallen Defender
@@ -165,7 +198,7 @@ lib.TokenItems = {
     [29766] = { 29074, 29063, 29069, 29046, 29030, 29036, 29042 },  -- Leggings of the Fallen Champion
     [32385] = { 28791, 28790, 28793, 28792 },                       -- Magtheridon's Head
     [32386] = { 28791, 28790, 28793, 28792 },                       -- Magtheridon's Head
-    -- TBC Classic P2
+    -- TBC Classic P2 (T5)
     [30243] = { 30120, 30115, 30161, 30152, 30228, 30219, 30233 },  -- Helm of the Vanquished Defender
     [30244] = { 30141, 30206, 30212 },                              -- Helm of the Vanquished Hero
     [30242] = { 30125, 30136, 30131, 30146, 30166, 30171, 30190 },  -- Helm of the Vanquished Champion
@@ -208,4 +241,39 @@ lib.TokenItems = {
     [34853] = { 34527, 34541, 34528, 34487, 34485, 34488 },         -- Belt of the Forgotten Conqueror
     [34854] = { 34549, 34546, 34547, 34543, 34542, 34545 },         -- Belt of the Forgotten Protector
     [34855] = { 34557, 34558, 34554, 34555, 34556 },                -- Belt of the Forgotten Vanquisher
+    -- WOTLK Classic P1 (T7)
+    -- 10 player
+    [40616] = { 39496, 39514, 39521, 39628, 39635, 39640 },         -- Helm of the Lost Conqueror
+    [40617] = { 39605, 39610, 39578, 39583, 39594, 39602 },         -- Helm of the Lost Protector
+    [40618] = { 39531, 39545, 39553, 39561, 39491, 39619, 39625 },  -- Helm of the Lost Vanquisher
+    [40613] = { 39500, 39519, 39530, 39632, 39634, 39639 },         -- Gloves of the Lost Conqueror
+    [40614] = { 39609, 39622, 39582, 39591, 39593, 39601 },         -- Gloves of the Lost Protector
+    [40615] = { 39543, 39544, 39557, 39560, 39495, 39618, 39624 },  -- Gloves of the Lost Vanquisher
+    [40610] = { 39497, 39515, 39523, 39629, 39633, 39638 },         -- Chestguard of the Lost Conqueror
+    [40611] = { 39606, 39611, 39579, 39588, 39592, 39597 },         -- Chestguard of the Lost Protector
+    [40612] = { 39538, 39547, 39554, 39558, 39492, 39617, 39623 },  -- Chestguard of the Lost Vanquisher
+    [40619] = { 39498, 39517, 39528, 39630, 39636, 39641 },         -- Leggings of the Lost Conqueror
+    [40620] = { 39607, 39612, 39580, 39589, 39595, 39603 },         -- Leggings of the Lost Protector
+    [40621] = { 39539, 39546, 39555, 39564, 39493, 39620, 39626 },  -- Leggings of the Lost Vanquisher
+    [40622] = { 39499, 39518, 39529, 39631, 39637, 39642 },         -- Spaulders of the Lost Conqueror
+    [40623] = { 39608, 39613, 39581, 39590, 39596, 39604 },         -- Spaulders of the Lost Protector
+    [40624] = { 39542, 39548, 39556, 39565, 39494, 39621, 39627 },  -- Spaulders of the Lost Vanquisher
+    [44569] = { 44582 },                                            -- Key to the Focusing Iris
+    -- 25 player
+    [40631] = { 40421, 40447, 40456, 40571, 40576, 40581 },         -- Crown of the Lost Conqueror
+    [40632] = { 40528, 40546, 40505, 40510, 40516, 40521 },         -- Crown of the Lost Protector
+    [40633] = { 40461, 40467, 40473, 40499, 40416, 40554, 40565},   -- Crown of the Lost Vanquisher
+    [40628] = { 40420, 40445, 40454, 40570, 40575, 40580 },         -- Gauntlets of the Lost Conqueror
+    [40629] = { 40527, 40545, 40504, 40509, 40515, 40520 },         -- Gauntlets of the Lost Protector
+    [40630] = { 40460, 40466, 40472, 40496, 40415, 40552, 40563 },  -- Gauntlets of the Lost Vanquisher
+    [40625] = { 40423, 40449, 40458, 40569, 40574, 40579 },         -- Breastplate of the Lost Conqueror
+    [40626] = { 40525, 40544, 40503, 40508, 40514, 40523 },         -- Breastplate of the Lost Protector
+    [40627] = { 40463, 40469, 40471, 40495, 40418, 40550, 40559 },  -- Breastplate of the Lost Vanquisher
+    [40634] = { 40422, 40448, 40457, 40572, 40577, 40583 },         -- Legplates of the Lost Conqueror
+    [40635] = { 40529, 40547, 40506, 40512, 40517, 40522 },         -- Legplates of the Lost Protector
+    [40636] = { 40462, 40468, 40493, 40500, 40417, 40556, 40567 },  -- Legplates of the Lost Vanquisher
+    [40637] = { 40424, 40450, 40459, 40573, 40578, 40584 },         -- Mantle of the Lost Conqueror
+    [40638] = { 40530, 40548, 40507, 40513, 40518, 40524 },         -- Mantle of the Lost Protector
+    [40639] = { 40465, 40470, 40494, 40502, 40419, 40557, 40568 },  -- Mantle of the Lost Vanquisher
+    [44577] = { 44581},                                             -- Heroic Key to the Focusing Iris
 }
